81ceeba2223517fc93bfd9c08eeea07be30a65a3
mdadilfaiz48 <mdadilfaiz48@gmail.com>
2026-06-06 20:28:59 +0530

    add comment in validationFilter.java from intellij ide

--- src/main/java/com/adil/filter/ValidationFilter.java
+++ src/main/java/com/adil/filter/ValidationFilter.java
@@ -25,7 +25,7 @@ public class ValidationFilter extends HttpFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
+		// this properties for display error message
 		String sid_error = "", sname_error = "", semail_error = "", sage_error = "", smobile_error = "";
 
 		try {
